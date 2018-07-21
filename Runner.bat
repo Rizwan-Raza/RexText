a02ddb7888946a5677fe77be14c3e7c829e48c3d
Rex Terminous <rizwan.raza987@gmail.com>
2018-07-21 13:12:23 +0530

    Update Runner.bat

--- Runner.bat
+++ Runner.bat
@@ -2,7 +2,6 @@
 color 0a
 prompt Rex:$g 
 REM runs only if javac command is added to the path variable
-REM runs only if javac command is added to the path variable
 :label
 cls
 javac com/rex/text/TextEditor.java
